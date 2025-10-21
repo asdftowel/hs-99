bac0cb5227fafb7e2f17f8d00abed4208dcf6d7e
Alexey <belochka31012005@gmail.com>
2025-10-21 10:12:12 +0300

    Switch to block comments

--- solutions/Problems20.hs
+++ solutions/Problems20.hs
@@ -1,16 +1,18 @@
--- Copyright 2025 asdftowel
+{-
+Copyright 2025 asdftowel
 
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
---     http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-}
 
 module Problems20 where
 
@@ -18,13 +20,15 @@ import Problems10 (rle)
 
 data ElementGroup x = Single x | Multiple Int x deriving Show
 
--- Some replacements for Prelude functions:
--- myReverse from Problems10
--- list and tuple indexing can be trivially implemented
--- through pattern matching
--- myReplicate :: Int -> x -> [x]
--- myReplicate 0 _ = []
--- myReplicate n x = x:(myReplicate (n-1) x)
+{-
+Some replacements for Prelude functions:
+myReverse from Problems10
+list and tuple indexing can be trivially implemented
+through pattern matching
+myReplicate :: Int -> x -> [x]
+myReplicate 0 _ = []
+myReplicate n x = x:(myReplicate (n-1) x)
+-}
 
 rle' :: Eq x => [x] -> [ElementGroup x]
 rle' xs = [
